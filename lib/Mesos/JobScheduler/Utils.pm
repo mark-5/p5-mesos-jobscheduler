ffcd9a03b06e2285c760a0eab4544a603dda7353
mark-5 <maflick88@gmail.com>
2015-08-07 23:21:08 -0400

    add json utils

--- lib/Mesos/JobScheduler/Utils.pm
+++ lib/Mesos/JobScheduler/Utils.pm
@@ -4,13 +4,34 @@ use strict;
 use warnings;
 use DateTime;
 use DateTime::Format::RFC3339;
-use JSON qw(to_json);
+use JSON qw();
 use base 'Exporter::Tiny';
-our @EXPORT_OK = qw(now psgi_json);
+our @EXPORT_OK = qw(
+    decode_json
+    encode_json
+    now
+    psgi_json
+);
 
 sub now {
     my (%args) = @_;
     return DateTime->now(time_zone => 'UTC');
+}
+
+sub decode_json { goto &JSON::decode_json }
+
+sub encode_json {
+    my ($object, $opts) = @_;
+    local *DateTime::TO_JSON = sub {
+        return DateTime::Format::RFC3339->format_datetime($_[0]);
+    } unless DateTime->can('TO_JSON');
+    return JSON::to_json($object, {
+        allow_blessed   => 1,
+        allow_nonref    => 1,
+        canonical       => 1,
+        convert_blessed => 1,
+        %{$opts||{}},
+    });
 }
 
 sub psgi_json {
@@ -18,19 +39,10 @@ sub psgi_json {
     $opts = {map {($_ => $opts->{$_})x!! $opts->{$_}} keys %{$opts||{}}};
     my $status = delete $opts->{status} // 200;
 
-    local *DateTime::TO_JSON = sub {
-        return DateTime::Format::RFC3339->format_datetime($_[0]);
-    } unless DateTime->can('TO_JSON');
-
     return [
         $status,
         ['Content-Type' => 'application/json'],
-        [to_json($object, {
-            canonical       => 1,
-            allow_blessed   => 1,
-            convert_blessed => 1,
-            %$opts,
-        })],
+        [encode_json($object, $opts)],
     ],
 }
 
